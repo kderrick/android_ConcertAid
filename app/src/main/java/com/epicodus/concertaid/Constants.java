8d5bd02723df621c95ef0e487492695bcff06cd5
Kyle <kderrick1988@gmail.com>
2016-04-27 11:20:58 -0700

    add BandsInTownApi ID to Constants.java and gradle properties

---
+++ app/src/main/java/com/epicodus/concertaid/Constants.java
@@ -0,0 +1,9 @@
+package com.epicodus.concertaid;
+
+/**
+ * Created by kylederrick on 4/27/16.
+ */
+public class Constants {
+
+    public static final String BANDS_IN_TOWN_ID = BuildConfig.BANDS_IN_TOWN_ID;
+}
