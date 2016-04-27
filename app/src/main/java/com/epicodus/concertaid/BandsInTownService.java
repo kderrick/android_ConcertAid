43c316e15daac84dda8a825cdbf5d9181a2f62cd
Kyle <kderrick1988@gmail.com>
2016-04-27 15:35:08 -0700

    add BandsInTownService with fucntioning query and DisplayActivity can receive and log query response

--- app/src/main/java/com/epicodus/concertaid/BandsInTownService.java
+++ app/src/main/java/com/epicodus/concertaid/BandsInTownService.java
@@ -21,8 +21,9 @@ public class BandsInTownService {
                 .build();
 
         HttpUrl.Builder urlBuilder = HttpUrl.parse(Constants.BANDS_IN_TOWN_BASE_URL).newBuilder();
-        urlBuilder.addPathSegment(artist + "/");
-        urlBuilder.addPathSegment("events/recommended");
+        urlBuilder.addPathSegment(artist);
+        urlBuilder.addPathSegment("events");
+        urlBuilder.addPathSegment("recommended");
         urlBuilder.addQueryParameter("location", city + "," + state);
         urlBuilder.addQueryParameter("radius", "25");
         urlBuilder.addQueryParameter("app_id", Constants.BANDS_IN_TOWN_ID);
@@ -30,7 +31,7 @@ public class BandsInTownService {
         urlBuilder.addQueryParameter("format", "json");
 
 //        http://api.bandsintown.com/artists/M83/events/recommended?location=city,state&radius=25&app_id=kderrick&api_version=2.0&format=json
-
+//        http://api.bandsintown.com/artists/M83%2F/events%2Frecommended?location=Portland,Or&radius=25&app_id=kderrick&api_version=2.0&format=json
         String url = urlBuilder.build().toString();
         Log.d(TAG, url);
         Request request = new Request.Builder()
--- app/src/main/java/com/epicodus/concertaid/DisplayActivity.java
+++ app/src/main/java/com/epicodus/concertaid/DisplayActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
@@ -11,11 +12,16 @@ import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.io.IOException;
+
 import butterknife.Bind;
 import butterknife.ButterKnife;
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.Response;
 
 public class DisplayActivity extends AppCompatActivity {
-
+    public static final String TAG = DisplayActivity.class.getSimpleName();
     @Bind(R.id.tvDisplayZipCode) TextView mTVDisplayZipCode;
     @Bind(R.id.lvDisplayConcerts) ListView mLVDisplayConcerts;
     private String[] concerts = new String[] {"Concert1", "COncert2", "Concert3", "Concert4", "Concert5", "Conert6", "Concert7"};
@@ -28,13 +34,35 @@ public class DisplayActivity extends AppCompatActivity {
         ButterKnife.bind(this);
 
 
+
         Intent intent = getIntent();
-        String userZip = intent.getStringExtra("userZip");
-        mTVDisplayZipCode.setText("Concerts near " + userZip);
+        String userArtist = intent.getStringExtra("userArtist");
+        String userCity = intent.getStringExtra("userCity");
+        String userState = intent.getStringExtra("userState");
+        getEvents(userArtist, userCity, userState);
 
-        ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, concerts);
-        mLVDisplayConcerts.setAdapter(adapter);
-
-
+//        ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, concerts);
+//        mLVDisplayConcerts.setAdapter(adapter);
     }
+    private void getEvents(String userArtist, String userCity, String userState) {
+        BandsInTownService.findEvents(userArtist, userCity, userState, new Callback() {
+            @Override
+            public void onFailure(Call call, IOException e) {
+                e.printStackTrace();
+            }
+
+            @Override
+            public void onResponse(Call call, Response response) throws IOException {
+                try {
+                    String jsonData = response.body().string();
+                    if (response.isSuccessful()) {
+                        Log.v(TAG, jsonData);
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+    }
+
 }
