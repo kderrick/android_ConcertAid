97aca9f5033a5c1ed1a77a6cb1ecabd22d565859
Kyle <kderrick1988@gmail.com>
2016-05-02 13:00:21 -0700

    WIP: Add PagerAdapter to EventDetailFragment

--- app/src/main/java/com/epicodus/concertaid/adapters/EventPagerAdapter.java
+++ app/src/main/java/com/epicodus/concertaid/adapters/EventPagerAdapter.java
@@ -1,7 +1,39 @@
 package com.epicodus.concertaid.adapters;
 
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+
+import android.support.v4.app.Fragment;
+
+import com.epicodus.concertaid.models.Event;
+import com.epicodus.concertaid.ui.EventDetailFragment;
+
+import java.util.ArrayList;
+
 /**
  * Created by kylederrick on 5/2/16.
  */
-public class EventPagerAdapter {
+public class EventPagerAdapter extends FragmentPagerAdapter {
+    private ArrayList<Event> mEvents;
+
+    public EventPagerAdapter(FragmentManager fm, ArrayList<Event> events) {
+        super(fm);
+        mEvents = events;
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        return EventDetailFragment.newInstance(mEvents.get(position));
+    }
+
+    @Override
+    public int getCount() {
+        return mEvents.size();
+    }
+
+    @Override
+    public CharSequence getPageTitle(int position) {
+        return mEvents.get(position).getEventTitle();
+    }
+
 }
