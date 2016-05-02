58045730f47ee60b470a069634bf64211fdb4cb7
Kyle <kderrick1988@gmail.com>
2016-05-02 16:37:55 -0700

    add click listener to ViewHolder itemView to start RestaurantDetailActivity

--- app/src/main/java/com/epicodus/concertaid/adapters/EventListAdapter.java
+++ app/src/main/java/com/epicodus/concertaid/adapters/EventListAdapter.java
@@ -1,6 +1,7 @@
 package com.epicodus.concertaid.adapters;
 
 import android.content.Context;
+import android.content.Intent;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -10,6 +11,9 @@ import android.widget.TextView;
 
 import com.epicodus.concertaid.R;
 import com.epicodus.concertaid.models.Event;
+import com.epicodus.concertaid.ui.EventDetailActivity;
+
+import org.parceler.Parcels;
 
 import java.util.ArrayList;
 
@@ -46,23 +50,29 @@ public class EventListAdapter extends RecyclerView.Adapter<EventListAdapter.Even
     }
 
     public class EventViewHolder extends RecyclerView.ViewHolder {
-        @Bind(R.id.eventImageView)
-        ImageView mEventImageView;
-        @Bind(R.id.eventNameTextView)
-        TextView mEventNameTextView;
-        @Bind(R.id.detailsTextView)
-        TextView mDetailsTextView;
+        @Bind(R.id.eventImageView) ImageView mEventImageView;
+        @Bind(R.id.eventNameTextView) TextView mEventNameTextView;
+        @Bind(R.id.detailsTextView) TextView mDetailsTextView;
         private Context mContext;
 
         public EventViewHolder(View itemView) {
             super(itemView);
-            ButterKnife.bind(this, itemView);
             mContext = itemView.getContext();
+            ButterKnife.bind(this, itemView);
+            itemView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    int itemPosition = getLayoutPosition();
+                    Intent intent = new Intent(mContext, EventDetailActivity.class);
+                    intent.putExtra("position", itemPosition + "");
+                    intent.putExtra("events", Parcels.wrap(mEvents));
+                    mContext.startActivity(intent);
+                }
+            });
         }
-
         public void bindEvent(Event event) {
             mEventNameTextView.setText(event.getEventTitle());
-//          mDetailsTextView.setText(event.getEventTitle().get(0));
+          mDetailsTextView.setText(event.getEventTitle());
         }
     }
 }
