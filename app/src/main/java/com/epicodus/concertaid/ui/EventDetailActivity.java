80c04b543163ee7a3dd46411709865390a0d421b
Kyle <kderrick1988@gmail.com>
2016-05-02 16:48:58 -0700

    WIP: fragment pager adapter

--- app/src/main/java/com/epicodus/concertaid/ui/EventDetailActivity.java
+++ app/src/main/java/com/epicodus/concertaid/ui/EventDetailActivity.java
@@ -1,15 +1,34 @@
 package com.epicodus.concertaid.ui;
 
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.epicodus.concertaid.R;
+import com.epicodus.concertaid.adapters.EventPagerAdapter;
+import com.epicodus.concertaid.models.Event;
+
+import org.parceler.Parcels;
+
+import java.util.ArrayList;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
 
 public class EventDetailActivity extends AppCompatActivity {
+    @Bind(R.id.viewPager) ViewPager mViewPager;
+    private EventPagerAdapter adapterViewPager;
+    ArrayList<Event> mEvents = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_event_detail);
+        ButterKnife.bind(this);
+        mEvents = Parcels.unwrap(getIntent().getParcelableExtra("events"));
+        int startingPosition = Integer.parseInt(getIntent().getStringExtra("position"));
+        adapterViewPager = new EventPagerAdapter(getSupportFragmentManager(), mEvents);
+        mViewPager.setAdapter(adapterViewPager);
+        mViewPager.setCurrentItem(startingPosition);
     }
 }
