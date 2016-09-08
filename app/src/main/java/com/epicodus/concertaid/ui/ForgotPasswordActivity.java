9738aba2f761321ae34d67ea0ec6f87ec1fbd4f7
Kyle <kderrick1988@gmail.com>
2016-09-08 16:02:42 -0700

    add ForgotPasswordActivity

---
+++ app/src/main/java/com/epicodus/concertaid/ui/ForgotPasswordActivity.java
@@ -0,0 +1,14 @@
+package com.epicodus.concertaid.ui;
+
+import android.os.Bundle;
+
+import com.epicodus.concertaid.R;
+
+public class ForgotPasswordActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_forgot_password);
+    }
+}
