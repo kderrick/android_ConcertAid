636d3426a3b4548f0b69eafd4c3a5fe28bb13a60
Kyle <kderrick1988@gmail.com>
2016-09-15 16:32:07 -0700

    WIP: update layout for UserProfileActivity

--- app/src/main/java/com/epicodus/concertaid/ui/UserProfileActivity.java
+++ app/src/main/java/com/epicodus/concertaid/ui/UserProfileActivity.java
@@ -1,15 +1,27 @@
 package com.epicodus.concertaid.ui;
 
+import android.graphics.Typeface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 import com.epicodus.concertaid.R;
 
+import butterknife.Bind;
+import butterknife.ButterKnife;
+
 public class UserProfileActivity extends AppCompatActivity {
+
+    @Bind(R.id.summaryTextView) TextView mSummaryTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_profile);
+        ButterKnife.bind(this);
+
+        //SETS FONT FOR TITLE
+        Typeface tf = Typeface.createFromAsset(getAssets(), "fonts/MUSICNET.ttf");
+        mSummaryTextView.setTypeface(tf);
     }
 }
