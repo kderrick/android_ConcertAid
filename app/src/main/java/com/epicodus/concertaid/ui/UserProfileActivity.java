3dc610c97a5bdea97b892548a781ff1cd33c61b9
Kyle <kderrick1988@gmail.com>
2016-09-20 20:32:36 -0700

    WIP: Adding AlertDialogue for deleting account

--- app/src/main/java/com/epicodus/concertaid/ui/UserProfileActivity.java
+++ app/src/main/java/com/epicodus/concertaid/ui/UserProfileActivity.java
@@ -1,6 +1,8 @@
 package com.epicodus.concertaid.ui;
 
+import android.content.DialogInterface;
 import android.graphics.Typeface;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -23,16 +25,32 @@ public class UserProfileActivity extends AppCompatActivity implements View.OnCli
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_profile);
         ButterKnife.bind(this);
+        mDeleteAccountButton.setOnClickListener(this);
 
         //SETS FONT FOR TITLE
         Typeface tf = Typeface.createFromAsset(getAssets(), "fonts/MUSICNET.ttf");
         mSummaryTextView.setTypeface(tf);
+
     }
 
     @Override
     public void onClick(View view) {
         if(view == mDeleteAccountButton) {
-
+            AlertDialog.Builder builder = new AlertDialog.Builder(UserProfileActivity.this);
+            builder.setMessage(R.string.dialog_message)
+                    .setTitle(R.string.dialog_title);
+            builder.setPositiveButton(R.string.delete, new DialogInterface.OnClickListener() {
+                public void onClick(DialogInterface dialog, int id) {
+                    // User clicked OK button
+                }
+            });
+            builder.setNegativeButton(R.string.cancel, new DialogInterface.OnClickListener() {
+                public void onClick(DialogInterface dialog, int id) {
+                    // User cancelled the dialog
+                }
+            });
+            AlertDialog dialog = builder.create();
+            System.out.println("CLICKED IT");
         }
     }
 }
